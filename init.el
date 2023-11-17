f3f6b5c63b6dc933c7701f34fc0103d978b970fd
lics6 <lics6@asiainfo.com>
2023-11-17 18:04:13 +0800

    添加pyim，采用purcel的package的管理策略

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
 
 (require 'init-vterm)
 
+(require 'init-pyim)
 
 ;;(require 'init-eaf)
 
---
+++ lisp/init-pyim.el
@@ -0,0 +1,20 @@
+(maybe-require-package  'pyim)
+(maybe-require-package 'pyim-basedict)
+(maybe-require-package 'pyim-cregexp-utils)
+
+;;设置全拼
+(pyim-default-scheme 'quanpin)
+
+;; 设置 pyim 探针
+;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;; 我自己使用的中英文动态切换规则是：
+;; 1. 光标只有在注释里面时，才可以输入中文。
+;; 2. 光标前是汉字字符时，才能输入中文。
+;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+(setq-default pyim-english-input-switch-functions
+              '(pyim-probe-dynamic-english
+                pyim-probe-isearch-mode
+                pyim-probe-program-mode
+                pyim-probe-org-structure-template))
+
+(provide 'init-pyim)
