dd69d7253b2174caffca7b08f03018271449bff7
lics6 <once_key@126.com>
2024-03-12 09:59:58 +0800

    evil下org支持tab

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -9,6 +9,9 @@
   :ensure t
   :init
   (setq evil-disable-insert-state-bindings t)
+  :config
+  (define-key evil-normal-state-map (kbd "<tab>") 'org-cycle)
+
   :hook (after-init . evil-mode)
   )
 (provide 'init-evil)
