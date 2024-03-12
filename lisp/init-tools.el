a975043295494263ddf4b2237a6f14ead1686ff2
lics6 <once_key@126.com>
2024-03-12 10:00:55 +0800

    添加whichkey

--- lisp/init-tools.el
+++ lisp/init-tools.el
@@ -5,4 +5,11 @@
 
 (use-package try
   :ensure t)
+
+(use-package which-key
+  :ensure t
+  :hook (after-init . which-key-mode)
+  :custom
+  (which-key-idle-delay 0.5)
+  (which-key-add-column-padding 1))
 (provide 'init-tools)
